31303382cc1f4b52261249065373b531a79216cb
leochok <stalh.leo@gmail.com>
2025-02-01 10:51:39 +0100

    Amélioration complete + ajout d'un classement par pseudo utilisateur

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Init
+let topTen = [];
+let sortListUsers = [];
+let rank = "#";
+
 refreshCounter();
+refreshTable();
 
 //Functions
 function refreshCounter() {
@@ -10,6 +15,47 @@ function refreshCounter() {
     });
 }
 
+function eraseTable() {
+  document.getElementById("user").innerHTML = "";
+  document.getElementById("usercount").innerHTML = "";
+  topTen = [];
+}
+
+function refreshTable() {
+  fetch("https://clickit-back.vercel.app/users")
+    .then((response) => response.json())
+    .then((data) => {
+      let dataUsers = data.listUsers;
+      let listUsers = [];
+      for (let i = 0; i < dataUsers.length; i++) {
+        const element = dataUsers[i];
+        listUsers.push(element);
+      }
+
+      sortListUsers = listUsers.sort(({ count: a }, { count: b }) => b - a);
+
+      for (let j = 0; j < 10; j++) {
+        topTen.push(sortListUsers[j]);
+      }
+
+      for (let x = 0; x < topTen.length; x++) {
+        const user = topTen[x];
+        document.getElementById("user").innerHTML += `
+        <div>
+      ${user.username}
+      </div>
+      `;
+        document.getElementById("usercount").innerHTML += `
+        <div>
+      ${user.count}
+      </div>
+      `;
+      }
+      document.getElementById("rank").textContent = rank;
+      document.getElementById("people").textContent = listUsers.length;
+    });
+}
+
 // Au click du bouton
 document.getElementById("button").addEventListener("click", () => {
   fetch("https://clickit-back.vercel.app/add")
@@ -17,6 +63,23 @@ document.getElementById("button").addEventListener("click", () => {
     .then((data) => {
       if (data) {
         refreshCounter();
+
+        // Si username rentré !
+
+        let username = document.getElementById("usernameTextField").value;
+
+        if (username) {
+          fetch(`https://clickit-back.vercel.app/users/add/${username}`)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data) {
+                rank =
+                  sortListUsers.findIndex((x) => x.username == username) + 1;
+                eraseTable();
+                refreshTable();
+              }
+            });
+        }
       }
     });
 });
