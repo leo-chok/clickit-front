7ab1097fd0ac69d9c0f9311ba6fa2b15ad8a9c9c
leochok <stalh.leo@gmail.com>
2025-02-04 10:00:17 +0100

    save

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Init
 let topTen = [];
 let sortListUsers = [];
-let rank = "#";
+let rank = `#`;
 let listUsers = [];
 
 refreshCounter();
 refreshTable();
-
+// setInterval(refreshCounter, 5000);
+// setInterval(refreshTable, 5000);
 //Functions
 function refreshCounter() {
   fetch("https://clickit-back.vercel.app/")
     .then((response) => response.json())
     .then((data) => {
+      console.log("refreshCounter");
       document.getElementById("counter").textContent = data.data[0].count;
     });
 }
@@ -27,8 +29,11 @@ function refreshTable() {
     .then((response) => response.json())
     .then((data) => {
       eraseTable();
+
       let dataUsers = data.listUsers;
       let listUsers = [];
+      let numberOfUsers = listUsers.length;
+
       for (let i = 0; i < dataUsers.length; i++) {
         const user = dataUsers[i];
         listUsers.push(user);
@@ -36,7 +41,6 @@ function refreshTable() {
 
       // Trie de la liste Users par nbre de clics
       sortListUsers = listUsers.sort(({ count: a }, { count: b }) => b - a);
-
 
       // Ajout des 10 premiers dans le tableau Top 10
       for (let j = 0; j < 10; j++) {
@@ -46,14 +50,18 @@ function refreshTable() {
       // Création du tableau Top 10
       for (let x = 0; x < topTen.length; x++) {
         const user = topTen[x];
-        document.getElementById("user").innerHTML += `<div>${user.username}</div>`;
-        document.getElementById("usercount").innerHTML += `<div>${user.count}</div>`;
+        document.getElementById(
+          "user"
+        ).innerHTML += `<div>${user.username}</div>`;
+        document.getElementById(
+          "usercount"
+        ).innerHTML += `<div>${user.count}</div>`;
       }
 
-      // Création durank sur le nombre d'users complet.
-      document.getElementById("rank").textContent = rank;
-      document.getElementById("people").textContent = listUsers.length;
+      // Création du rank sur le nombre d'users complet.
     });
+  document.getElementById("rank").textContent = rank;
+  document.getElementById("people").textContent = numberOfUsers;
 }
 
 // Au click du bouton
@@ -66,14 +74,18 @@ document.getElementById("button").addEventListener("click", () => {
 
         // Si username rentré !
 
-        let username = document.getElementById("usernameTextField").value.toLowerCase();
+        let username = document
+          .getElementById("usernameTextField")
+          .value.toLowerCase();
 
         if (username) {
           fetch(`https://clickit-back.vercel.app/users/add/${username}`)
             .then((response) => response.json())
             .then((data) => {
               if (data) {
-                rank = sortListUsers.findIndex((x) => x.username === username) + 1;
+                rank =
+                  sortListUsers.findIndex((x) => x.username === username) + 1;
+                document.getElementById("rank").textContent = rank;
                 refreshTable();
               }
             });
